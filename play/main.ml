5321e5b58f8e77ea889266bd6e217d7655e158fb
Pearl Oyewole <pearloyewole1@gmail.com>
2025-07-25 15:53:31 +0000

    start button

--- play/main.ml
+++ play/main.ml
@@ -5,8 +5,9 @@ open! Bonsai.Let_syntax
 let component = 
   let%sub title = Bonsai.const (Vdom.Node.text "PC City") in
   let%sub grid = Grid.component in 
+  let%sub button = Bonsai.const (Vdom.Node.button [Vdom.Node.text "Start Game"]) in 
 
-  let%arr title = title and grid = grid in 
-  View.vbox [title; grid] 
+  let%arr title = title and grid = grid and button = button in 
+  View.vbox [title; button; grid] 
 
 let () = Bonsai_web.Start.start component
