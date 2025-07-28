5abaddcd51574100d06d0a77bd60de53ea3e9a4b
Pearl Oyewole <pearloyewole1@gmail.com>
2025-07-28 19:53:23 +0000

    right sidebar completed

--- play/main.ml
+++ play/main.ml
@@ -15,9 +15,20 @@ let stats_items = [
   ("Day: ", default_value)
 ]
 
+(*switch this list with the list in the backend *)
+let opinion_messages = [
+  "The power is out again...";
+  "The hospitals are so fugazi(bad), I hope no one gets sick!";
+  "Who's reaching my illegal function! There's no 12(police) to stop it!";
+  "Bro I love the new education policy... Everyone is so locked in (focused)!";
+  "These high taxes have me feeling a certain type of way (upset). Might leave soon...idk";
+]
+
+
 let component : Node.t Bonsai.Computation.t =
   Bonsai.const (
 
+   Node.div ~attrs:[ Attr.class_ "sidebar" ] [
   (*City stats*)
     Node.div ~attrs:[Attr.class_ "section-box"]
     [
@@ -28,14 +39,21 @@ let component : Node.t Bonsai.Computation.t =
           Node.span ~attrs: [Attr.class_ "stats-label"] [Node.text label]; 
           Node.span ~attrs: [Attr.class_ "legend-label"] [Node.text (string_of_int value)]
         ]))
-    ]
+    ]; 
 
     (*Public Opinion*)
-    
-    (* Node.div ~attrs: [Attr.class_ "section-box"]
+    Node.div ~attrs: [Attr.class_ "section-box"]
     [
-      Node.h3 ~attrs:[Attr.class_ "section-title"] [Node.text "Public Opinion"]
-    ] *)
+      Node.h3 ~attrs:[Attr.class_ "section-title"] [Node.text "Public Opinion"]; 
+      Node.div ~attrs: [Attr.class_ "public-opinion"] 
+      (List.map opinion_messages ~f:(fun(message)-> 
+        Node.div ~attrs: [Attr.class_ "public-item"] [
+          Node.span ~attrs: [Attr.class_ "public-label"] [Node.text message]
+        ]
+        ))
+      ]
+    ] ; 
+
   ) 
 
 end 
