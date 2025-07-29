0df42c5d46b17517bca1acc18a1e2e06d0908f97
Pearl Oyewole <pearloyewole1@gmail.com>
2025-07-29 13:58:54 +0000

    seperated left sidebar to city_result file

--- play/main.ml
+++ play/main.ml
@@ -6,57 +6,6 @@ open! Js_of_ocaml
 open Game_strategies_common_lib
 
 
-module City_results = struct
- let default_value = 0  (*to maintain variable state for now, christine change this when backend is connected*)
-let stats_items = [
-  ("Population: ", default_value);
-  ("Money: ", default_value);
-  ("Happiness: ", default_value); 
-  ("Day: ", default_value)
-]
-
-(*switch this list with the list in the backend *)
-let opinion_messages = [
-  "The power is out again...";
-  "The hospitals are so fugazi(bad), I hope no one gets sick!";
-  "Who's reaching my illegal function! There's no 12(police) to stop it!";
-  "Bro I love the new education policy... Everyone is so locked in (focused)!";
-  "These high taxes have me feeling a certain type of way (upset). Might leave soon...idk";
-]
-
-
-let component : Node.t Bonsai.Computation.t =
-  Bonsai.const (
-
-   Node.div ~attrs:[ Attr.class_ "sidebar" ] [
-  (*City stats*)
-    Node.div ~attrs:[Attr.class_ "section-box"]
-    [
-      Node.h3 ~attrs:[Attr.class_ "section-title"] [Node.text "City Stats"];
-      Node.div ~attrs:[Attr.class_ "city-stats"]
-      (List.map stats_items ~f:(fun (label, value)-> 
-        Node.div ~attrs: [Attr.class_ "stats-item"][
-          Node.span ~attrs: [Attr.class_ "stats-label"] [Node.text label]; 
-          Node.span ~attrs: [Attr.class_ "legend-label"] [Node.text (string_of_int value)]
-        ]))
-    ]; 
-
-    (*Public Opinion*)
-    Node.div ~attrs: [Attr.class_ "section-box"]
-    [
-      Node.h3 ~attrs:[Attr.class_ "section-title"] [Node.text "Public Opinion"]; 
-      Node.div ~attrs: [Attr.class_ "public-opinion"] 
-      (List.map opinion_messages ~f:(fun(message)-> 
-        Node.div ~attrs: [Attr.class_ "public-item"] [
-          Node.span ~attrs: [Attr.class_ "public-label"] [Node.text message]
-        ]
-        ))
-      ]
-    ] ; 
-
-  ) 
-
-end 
 let component = 
   let%sub game, set_game =
     Bonsai.state
@@ -75,7 +24,7 @@ let component =
 
   let%sub title = Bonsai.const (Node.h1 ~attrs:[Attr.class_ "title"] [Node.text "PC City"]) in
   let%sub grid = Grid.component ~game ~set_game ~selected_cell ~set_selected_cell in 
-  let%sub right_sidebar = City_results.component in 
+  let%sub right_sidebar = City_result.component in 
   let%sub left_sidebar = City_planner.component ~game ~set_game ~selected_cell in 
 
   let%arr title = title
