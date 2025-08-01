3d2344cff35041276f4462705e837d7e8d7d99c8
Pearl Oyewole <pearloyewole1@gmail.com>
2025-08-01 15:26:25 +0000

    error message fixes

--- play/error_modal.ml
+++ play/error_modal.ml
@@ -1,24 +1,29 @@
-open! Core 
+open! Core
 open! Bonsai_web
 open! Bonsai.Let_syntax
 open! Virtual_dom.Vdom
 open! Game_strategies_common_lib
 
-let component ~error_message ~set_error_message= 
+let component ~error_message ~set_error_message =
   let%arr error_message = error_message
-  and set_error_message = set_error_message
-  in
+  and set_error_message = set_error_message in
   match error_message with
   | None -> Node.none
   | Some msg ->
       Node.div
-        ~attrs:[Attr.class_ "modal-overlay"]
-        [ Node.div
-            ~attrs:[Attr.class_ "modal"]
-            [ Node.h2 [Node.text "Error"];
-              Node.p [Node.text msg];
+        ~attrs:
+          [
+            Attr.class_ "modal-overlay";
+            Attr.on_click (fun _ -> set_error_message None);
+          ]
+        [
+          Node.div
+            ~attrs:[ Attr.class_ "modal" ]
+            [
+              Node.h2 [ Node.text "Error" ];
+              Node.p [ Node.text msg ];
               Node.button
-                ~attrs:[Attr.on_click (fun _ -> set_error_message None)]
-                [Node.text "Close"]
-            ]
+                ~attrs:[ Attr.on_click (fun _ -> set_error_message None) ]
+                [ Node.text "Close" ];
+            ];
         ]
